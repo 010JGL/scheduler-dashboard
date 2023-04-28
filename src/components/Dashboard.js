8dd55e5d68e8f8a92c2089aa7f0b836a236570e1
Jeremy <010JGL@GMAIL.COM>
2023-04-28 12:54:18 -0400

    add websocket link

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,6 +12,8 @@ import {
   getInterviewsPerDay
 } from "helpers/selectors";
 
+import { setInterview } from "helpers/reducers";
+
 const data = [
   {
     id: 1,
@@ -59,10 +61,24 @@ class Dashboard extends Component {
         interviewers: interviewers.data
       });
     });
-
+    
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
     if (focused) {
       this.setState({ focused });
     }
+  }
+  componentWillUnmount() {
+    this.socket.close();
   }
 
   componentDidUpdate(previousProps, previousState) {
